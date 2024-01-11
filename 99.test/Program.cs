7633999c4c3ed04a50edb51d82549adcc0fc53a9
JongMin <qkjm4836@gmail.com>
2024-01-11 14:50:11 +0900

    초기 커밋.

---
+++ 99.test/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _99.test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userkey = int.Parse(Console.ReadLine());
+
+            List<int> list = new List<int>();
+            for (int i = 0; i <= userkey; i++)
+            {
+                list.Add(i);
+            }
+        }
+    }
+}
